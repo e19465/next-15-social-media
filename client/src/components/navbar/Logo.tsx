2a66fbcb6ecf4b38ed3e6d000dbb6ba57b198fb6
Sasindu Dilshan <sasindudil0002@gmail.com>
2024-06-30 01:17:59 +0530

    progress link added to navbar

--- client/src/components/navbar/Logo.tsx
+++ client/src/components/navbar/Logo.tsx
@@ -1,14 +1,14 @@
-import Link from "next/link";
+import { ProgressLink } from "../nprogress/NProgressHandler";
 
 const Logo = () => {
   return (
     <div className="block p-1 shadow-md rounded-md sm:w-full w-3/4">
-      <Link
+      <ProgressLink
         className="w-full h-full flex items-center justify-center no-underline blue-gradient_text text-sm md:text-xl font-bold"
         href="/"
       >
         SDSOCIAL
-      </Link>
+      </ProgressLink>
     </div>
   );
 };
